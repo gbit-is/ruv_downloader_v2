2aa0e7c4b2a24d5d164badad9a20f3e5ebb8264d
Guðrún Ólafsdóttir <Gudrun.Olafsdottir@landsbankinn.is>
2024-01-31 19:51:28 +0000

    wip

--- config.py
+++ config.py
@@ -3,10 +3,77 @@
 filsi_1 = {
     "show_id"   :   "35276",
     "show_url"  :   "https://www.ruv.is/krakkaruv/spila/filsi-og-velarnar/35276/",
-    "dl_folder" :   "output_folder",
-    "filename"  :   "Fílsi og Vélarnar - S01E{NUM} - {NAME}"
+    "dl_folder" :   "/Users/gudrun/barnaefni/filsi",
+    "filename"  :   "Fílsi og Vélarnar - S01E{NUM} - {NAME} ({ID})",
+    "identifier":   "ID"
 }
 
+hvolpasveitin_8 = {
+    "show_id"   :   "34358",
+    "show_url"  :   "https://www.ruv.is/krakkaruv/spila/hvolpasveitin-viii/34358/a7l7bi/",
+    "dl_folder" :   "ooooo",
+    "filename"  :   "Hvolpasveitin - S08E{NUM} - {NAME} ({ID})",
+    "identifier":   "ID"
+}
+
+dannitigur_1 = {
+        "show_id"   :   "18990",
+        "show_url"  :   "https://www.ruv.is/krakkaruv/spila/vinabaer-danna-tigurs/18990/",
+        "dl_folder" :   "...",
+        "filename"  :   "Danni Tigur - S01E{NUM} - {NAME} ({ID})",
+        "identifier":   "ID"
+}
+
+
+ljonidurri_1 = {
+    "show_id"   :   "34241",
+    "show_url"  :   "https://www.ruv.is/krakkaruv/spila/havaera-ljonid-urri/34241/",
+    "dl_folder" :   "asdf",
+    "filename"  :   "Háværa Ljónið Urrir - S01E{NUM} - {NAME} ({ID})",
+}
+
+
+
+bursti_1 = {
+
+        "show_id"   :   "34279",
+        "show_url"  :   "https://www.ruv.is/krakkaruv/spila/bursti/34279/",
+        "dl_folder" :   "...",
+        "filename"  :   "Bursti - S01E{NUM} - {NAME} ({ID})",
+        "identifier":   "ID"
+}
+
+jolasmastund = {
+        "show_id"   :   "35878",
+        "show_url"  :   "https://www.ruv.is/krakkaruv/spila/jola-smastund/35878/",
+        "dl_folder" :   "...",
+        "filename"  :   "Smástund - S01E{NUM} - {NAME} ({ID})",
+        "identifier":   "ID"
+}
+
+blaeja_2 = {
+        "show_id"   :   "34551",
+        "show_url"  :   "https://ruv.is/krakkaruv/spila/blaeja-ii/34551/",
+        "dl_folder" :   "...",
+        "filename"  :   "Blæja - S02E{NUM} - {NAME} ({ID})",
+        "identifier":   "ID"
+}
+
+hvolpasveit_1 = {
+        "show_id"   :   "34357",
+        "show_url"  :   "https://www.ruv.is/krakkaruv/spila/hvolpasveitin/34357/",
+        "dl_folder" :   "...",
+        "filename"  :   " - S01E{NUM} - {NAME} ({ID})",
+        "identifier":   "ID"
+}
+
+
+
+
+
+
+# https://ruv.is/krakkaruv/spila/blaeja-ii/34551/a9g44m
+# https://www.ruv.is/krakkaruv/spila/hvolpasveitin/34357/a7ktj1
 
 
 shows = [ filsi_1 ]
---
+++ prereqs.sh
@@ -0,0 +1,3 @@
+pip install requests
+pip install yt_dlp
+pip install ffmpeg # Not needed ?
